076b18443c38e6414dca07fc148b7327c9c2d391
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-04-06 20:32:23 +0300

    Reconfigure file sturcture

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class QuestionsConfig(AppConfig):
+class AppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'questions'
+    name = 'app'
