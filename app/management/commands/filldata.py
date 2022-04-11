383d5e3ca318699619561625e13eb24c44e7ae96
Alexander Dunaitsev <dunaitsev.alexander@gmail.com>
2022-04-12 01:43:11 +0300

    Implemented data fill command

--- app/management/commands/filldata.py
+++ app/management/commands/filldata.py
@@ -13,21 +13,26 @@ class Command(BaseCommand):
     RANDOM_API_KEY = '78c7df1074544d209e549dc135589056'
     RANDOM_TEXT_APY = 'https://randommer.io/api/Text/LoremIpsum'
     RANDOM_NAME_API = 'https://randommer.io/api/Name'
-    PARAGRAPHS_AMOUNT = 10
+    PARAGRAPHS_AMOUNT = 1
 
     SCALE = 5000
     USERS_NEEDS = 10000 // SCALE
     QUESTIONS_NEEDS = 100000 // SCALE
     ANSWERS_NEEDS = 1000000 // SCALE
-    TAGS_NEEDS = 10000 // SCALE
+    TAGS_NEEDS = 20000 * QUESTIONS_NEEDS // SCALE
     LIKES_NEEDS = 2000000 // SCALE
+
+    TITLE_LEN = 10
+    MIN_TEXT_LEN = 20
+    MAX_TEXT_LEN = 100
+    MAX_ANSWERS = 5
+    MAX_TAGS = 5
+    MAX_LIKES = 40
 
     def __init__(self):
         super().__init__()
         self.text_dataset = self.generate_words_dataset()
         self.names_set = self.generate_names_set()
-        self.users_set = []
-        self.profiles_set = []
 
     def generate_words_dataset(self):
         params = {'loremType': 'normal', 'type': 'paragraphs', 'number': self.PARAGRAPHS_AMOUNT}
@@ -49,38 +54,130 @@ class Command(BaseCommand):
 
     used_names = {}
 
-    def create_user(self):
-        name_choice = random.choice(self.names_set)
-        while name_choice in self.used_names:
-            name_choice = random.choice(self.names_set)
-        name_split = name_choice.split()
-        pwd = f'{random.choice(self.text_dataset)}{random.choice(self.text_dataset)}{random.choice(self.text_dataset)}'
-        user_dict_repr = {
-            'username': name_choice,
-            'first_name': name_split[0],
-            'last_name': name_split[1],
-            'password': pwd,
-            'email': f'{random.choice(self.text_dataset)}@domen.mail',
-            'is_staff': False,
-            'is_active': True,
-            'is_superuser': False,
-            'last_login': datetime.datetime.now(tz=timezone.utc),
-            'date_joined': datetime.date.today()
-        }
-        return user_dict_repr
+    def create_text_by_word_length(self, len):
+        result_string = str()
+        for i in range(len):
+            result_string = result_string + random.choice(self.text_dataset) + ' '
+        return result_string
 
     def create_users_and_ref_profiles(self):
-        self.users_set = []
-        self.profiles_set = []
-        for i in range(self.USERS_NEEDS):
-            user = User(**self.create_user())
-            user.save()
-            profile = Profile(user=user)
-            self.profiles_set.append(profile)
+        def create_user():
+            name_choice = random.choice(self.names_set)
+            while name_choice in self.used_names:
+                name_choice = random.choice(self.names_set)
+            name_split = name_choice.split()
+            pwd = f'{random.choice(self.text_dataset)}{random.choice(self.text_dataset)}{random.choice(self.text_dataset)}'
+            user_dict_repr = {
+                'username': name_choice,
+                'first_name': name_split[0],
+                'last_name': name_split[1],
+                'password': pwd,
+                'email': f'{random.choice(self.text_dataset)}@domen.mail',
+                'is_staff': False,
+                'is_active': True,
+                'is_superuser': False,
+                'last_login': datetime.datetime.now(tz=timezone.utc),
+                'date_joined': datetime.date.today()
+            }
+            return user_dict_repr
 
-        Profile.objects.bulk_create(self.profiles_set)
+        users_set = []
+        profiles_set = []
+        for i in range(self.USERS_NEEDS):
+            user = User(**create_user())
+            user.save()
+            users_set.append(user)
+            profile = Profile(user=user)
+            profiles_set.append(profile)
+
+        Profile.objects.bulk_create(profiles_set)
+
+    def create_questions(self, users):
+        def create_question(user):
+            question_fields = {
+                'title': self.create_text_by_word_length(self.TITLE_LEN),
+                'text': self.create_text_by_word_length(random.randint(self.MIN_TEXT_LEN, self.MAX_TEXT_LEN)),
+                'time': datetime.datetime.now(tz=timezone.utc),
+                'author': user
+            }
+            return question_fields
+
+        questions_set = []
+        for i in range(self.QUESTIONS_NEEDS):
+            question = Question(**create_question(random.choice(users)))
+            questions_set.append(question)
+
+        Question.objects.bulk_create(questions_set)
+
+    def create_answers(self, users, questions):
+        def create_answer(user, question):
+            answer_fields = {
+                'text': self.create_text_by_word_length(random.randint(self.MIN_TEXT_LEN, self.MAX_TEXT_LEN)),
+                'correct': random.choice([True, False]),
+                'time': datetime.datetime.now(tz=timezone.utc),
+                'question': question,
+                'author': user
+            }
+            return answer_fields
+
+        answers_created_indicate = self.ANSWERS_NEEDS
+        answers_set = []
+        while answers_created_indicate > 0:
+            one_question_answers_count = random.randint(0, self.MAX_ANSWERS)
+            answers_created_indicate -= one_question_answers_count
+            question = random.choice(questions)
+            for i in range(one_question_answers_count):
+                answer = Answer(**create_answer(random.choice(users), question))
+                answers_set.append(answer)
+
+        Answer.objects.bulk_create(answers_set)
+
+    def create_tags(self, questions):
+        def create_tag(question):
+            tag_fields = {
+                'tag_name': random.choice(self.text_dataset),
+                'question': question
+            }
+            return tag_fields
+
+        tags_created_indicate = self.TAGS_NEEDS
+        tags_set = []
+        while tags_created_indicate > 0:
+            one_question_tags_amount = random.randint(0, self.MAX_TAGS)
+            tags_created_indicate -= one_question_tags_amount
+            question = random.choice(questions)
+            for i in range(one_question_tags_amount):
+                tag = Tag(**create_tag(question))
+                tags_set.append(tag)
+
+        Tag.objects.bulk_create(tags_set)
+
+    def create_likes(self, users, questions):
+        def create_like(user, question):
+            like_fields = {
+                'user': user,
+                'question': question
+            }
+            return like_fields
+
+        likes_created_indicate = self.LIKES_NEEDS
+        likes_set = []
+        while likes_created_indicate > 0:
+            one_question_like_amount = random.randint(0, self.MAX_LIKES)
+            likes_created_indicate -= one_question_like_amount
+            question = random.choice(questions)
+            for i in range(one_question_like_amount):
+                like = Like(**create_like(random.choice(users), question))
+                likes_set.append(like)
+
+        Like.objects.bulk_create(likes_set)
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('FIRST COMMAND MESSAGE'))
         self.create_users_and_ref_profiles()
-        # self.create_profiles()
+        users = User.objects.all()
+        self.create_questions(users)
+        questions = Question.objects.all()
+        self.create_answers(users, questions)
+        self.create_tags(questions)
+        self.create_likes(users, questions)
